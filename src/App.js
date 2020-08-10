837d9f735bc70b1af18eeb437ce03920f2a2eb3c
Mohamed Goui <goui.mohamed01@gmail.com>
2020-08-10 15:12:20 +0100

    Trying to manipulate with JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image1 from '../src/image1.jpg';
+import '../src/style.css';
 
 function App() {
+  const styleDiv = {
+    border: "solid 1px black",
+    maxWidth: "100vw",
+  };
+  const styleImg = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%"
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style = {styleDiv}>
+      <h1 className="title red">Mohamed Goui</h1>
+      <br/>
+      <img src = {image1} style = {styleImg} />
+      <br/>
+      <img src = "/image2.jpg" style = {styleImg} />
     </div>
+    <video width = "320" height = "240" controls>
+      <source src="myVideo.mp4" type="video/mp4"/>
+    </video>
+    </>
   );
 }
 
